d55970427de73700d8274c643cf32dd78a20e25a
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-11-08 18:29:03 -0300

    task: remove create-react-app unused code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        fontSize: '4rem',
+        with: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+      }}
+    >
+      Let's code 💻
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
