4359cf0829a725d34f5c5ad56fbaefa630da768d
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-12 19:11:42 -0300

    Add image "Cadastro.jpg" in register page with css.

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,18 @@
+import styled from 'styled-components'
+import cadastro from '../assets/Cadastro.jpg'
+
+
+
 function Register() {
+
+    const Image = styled.img`
+        width:55%;
+    `;
+
     return (
-        <div>
-            Teste
+        <div className="Register">
+            <Image src={cadastro}/>
+            <div style={{"border": "2px solid green"}}>Teste</div>
         </div>
     )
 }
