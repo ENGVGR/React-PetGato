bd46c9d4b179419abcb21d5a724367b64846d714
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-14 11:15:08 -0300

    Alteraçoes no arquivo button.js: Foi reirado a funçao ButtonBackGroundPurple de dentro da funcao Button

--- src/components/button.js
+++ src/components/button.js
@@ -2,35 +2,35 @@ import styled from "styled-components";
 
 
 function Button(props) {
-    
-    function ButtonBackGroundPurple({Label}) {
-    
-        const Button = styled.button`
-            cursor: pointer;
-            font-size: 1.4em;
-            width: 8em;
-            height: 2em;
-            margin-top: 1.1em;
-            background-color: #C882B4;
-            color white;
-            border: none;
-            border-radius: 5px;
-            &:hover {
-                background-color: #BA66A3;
-                color: white;
-            }
-        `;
-
-        return (
-            <Button>{Label}</Button>
-        )
-    }
 
     if (props.BackGround === "Purple") {
         return (
             <ButtonBackGroundPurple Label={props.Label}/>
         )
     }
+}
+
+function ButtonBackGroundPurple({Label}) {
+    
+    const Button = styled.button`
+        cursor: pointer;
+        font-size: 1.4em;
+        width: 8em;
+        height: 2em;
+        margin-top: 1.1em;
+        background-color: #C882B4;
+        color white;
+        border: none;
+        border-radius: 5px;
+        &:hover {
+            background-color: #BA66A3;
+            color: white;
+        }
+    `;
+
+    return (
+        <Button>{Label}</Button>
+    )
 }
 
 export default Button
