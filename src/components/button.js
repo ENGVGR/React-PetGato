053b7bb031796616dfd441de4067176b428e2124
HCarrer <hmcarrer@gmail.com>
2021-11-15 14:08:01 -0300

    Pagina de redefinicao de senha visualmente implementada

--- src/components/button.js
+++ src/components/button.js
@@ -15,8 +15,10 @@ function ButtonBackGroundPurple({label}) {
     const Button = styled.button`
         cursor: pointer;
         font-size: 1.4em;
-        width: 8em;
+        width: auto;
         height: 2em;
+        padding-left: 1em;
+        padding-right: 1em;
         margin-top: 1.1em;
         background-color: #C882B4;
         color white;
