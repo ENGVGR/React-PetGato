f3f78164838ad1661c07a9e3f513109ceacf52bc
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-23 20:52:30 -0300

    Implementaçao da pagina de perfil

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -3,13 +3,45 @@ import foto from "../../assets/Cíntia Lorenzzo.jpg"
 import "./index.scss"
 import { Link } from "react-router-dom"
 import camera from "../../assets/icons/Icon awesome-camera.svg"
+import {InputProfile, LabelProfile} from "../../components/input"
+import { ButtonWhite } from "../../components/button"
+import { BottomPage } from "../../components/bottompage/index"
 
 function Profile() {
     return(
-        <div>
+        <div className="profile">
             <NavBar/>
-            <img className="perfil-photo" src={foto} alt="foto-de-perfil"/>
-            <Link className="perfil-photo-link" to="/"><img className="perfil-photo-icon" src={camera} alt="camera"/> Alterar foto de perfil</Link>
+            <div className="profile-div">
+                <div className="profile-photo-div">
+                    <img className="profile-photo" src={foto} alt="foto-de-perfil"/>
+                    <Link className="profile-photo-link" to="/"><img className="profile-photo-icon" src={camera} alt="camera"/>Alterar foto de perfil</Link>
+                </div>
+                <div className="profile-form-div">
+                    <div className="profile-title-div">
+                        <span className="profile-title">Sua conta</span>
+                        <span className="profile-title-2">Edite seu perfil</span>
+                    </div>
+                    <form className="profile-form">
+                        <div className="profile-input-div">
+                            <LabelProfile htmlFor="name">Nome</LabelProfile>
+                            <InputProfile id="name" required/>
+                            <LabelProfile htmlFor="new-password">Nova senha</LabelProfile>
+                            <InputProfile id="new-password" type="password"/>
+                            <span className="profile-input-span">Deixe em branco caso não queira alterar</span>
+                            <LabelProfile htmlFor="password">Senha atual</LabelProfile>
+                            <InputProfile id="password" type="password" required/>   
+                            <ButtonWhite>Salvar</ButtonWhite>
+                        </div>
+                        <div className="profile-input-div-2">
+                            <LabelProfile htmlFor="email">Email</LabelProfile>
+                            <InputProfile id="email" type="email" required/>
+                            <LabelProfile htmlFor="new-password-conf">Confirme sua senha</LabelProfile>
+                            <InputProfile id="new-password-conf" type="password"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <BottomPage/>
         </div>        
     )
 }
