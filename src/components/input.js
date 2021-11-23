a74eb8710fc91b8405ce63c21632b08e7dd92939
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-23 20:50:34 -0300

    Implementaçao de input e label destinados a pagina profile

--- src/components/input.js
+++ src/components/input.js
@@ -8,6 +8,14 @@ const Input = styled.input`
     text-align: center;
 `;
 
+const InputProfile = styled.input`
+    border: 1px solid #C882B4;
+    height: 1.7em;
+    font-size: 1.3em;
+    text-align: center;
+    min-width: 45vh;
+`;
+
 const Label = styled.label`
     font-size: 1.2em;
     width: 50%;
@@ -18,5 +26,14 @@ const Label = styled.label`
     margin-top: 0.6em
 `;
 
-export {Input, Label}
+const LabelProfile = styled.label`
+    font-size: 1.2em;
+    width: 50%;
+    display: block;
+    color: #C882B4;
+    font-weight: 700;
+    margin-top: 1em;
+`;
+
+export {Input,InputProfile, Label, LabelProfile}
 
