842d7ef043615ba3ef0435a247a96dcbe45ce6ec
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-22 23:21:55 -0300

    Implementado a foto de perfil

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,8 +1,16 @@
 import { NavBar } from "../../components/navbar/index"
+import foto from "../../assets/Cíntia Lorenzzo.jpg"
+import "./index.scss"
+import { Link } from "react-router-dom"
+import camera from "../../assets/icons/Icon awesome-camera.svg"
 
 function Profile() {
     return(
-        <NavBar/>
+        <div>
+            <NavBar/>
+            <img className="perfil-photo" src={foto} alt="foto-de-perfil"/>
+            <Link className="perfil-photo-link" to="/"><img className="perfil-photo-icon" src={camera} alt="camera"/> Alterar foto de perfil</Link>
+        </div>        
     )
 }
 
