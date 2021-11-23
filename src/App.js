9965cc5ce98018da69d08bbb9f4980aa2bd66b4f
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-22 21:00:54 -0300

    Rota para a recuperaçao do email, acessada pelo email do usuario

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import { Routes } from "react-router";
 import './index.css'
 import Register from './pages/register/index'
-import ResetPassword from './pages/resetPassword/index'
+import ForgotPassword from './pages/forgotPassword/index'
 import Login from "./pages/login";
+import ResetPassword from "./pages/resetPassword";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/registro" element={<Register/>}/>
-        <Route path="/recuperacao-de-senha" element={<ResetPassword/>}/>
+        <Route path="/recuperacao-de-senha" element={<ForgotPassword/>}/>
+        <Route path="/recuperar-senha/:id/:token" element={<ResetPassword/> }/>
       </Routes>
     </Router>
   )
