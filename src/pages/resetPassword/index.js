0315073d94eb3677a5eaa0dc15dbf5f404eb1fb5
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-23 21:03:07 -0300

    Melhorias no codigo

--- src/pages/resetPassword/index.js
+++ src/pages/resetPassword/index.js
@@ -14,8 +14,7 @@ function ResetPassword() {
     const [ErrorPassword, setErrorPassword] = useState(false)
     const [ErrorExp, setErrorExp] = useState(false)
     const [Confirmation, setConfirmation] = useState(false)
-    const { id } = useParams()
-    const { token } = useParams()
+    const { id, token } = useParams()
     
     
 
@@ -59,8 +58,8 @@ function ResetPassword() {
                 <Input id="password" type="password" value={Password} onChange={(event) => {setPassword(event.target.value)}} required minLength="8"/>
                 <Label htmlFor="password_confirm">Confirme sua senha{ErrorPassword?<span className="reset-password-span-error"> *Precisa ser a mesma senha!</span>:<span></span>}</Label>
                 <Input id="password_confirm" type="password" value={PasswordConfirm} onChange={(event) => {setPasswordConfirm(event.target.value)}} required minLength="8"/><br/>
-                {ErrorExp?<span className="reset-password-span-error"> *Token já utilizado ou expirado!</span>:<span></span>}
-                {Confirmation?<span className="reset-password-span-error"> *Senha alterada</span>:<span></span>}
+                {ErrorExp?<span className="reset-password-span-error"> *Token já utilizado ou expirado!</span>:<></>}
+                {Confirmation?<span className="reset-password-span-error"> *Senha alterada</span>:<></>}
                 <ButtonPurple>ALTERAR A SENHA</ButtonPurple>
             </form>
         </div>
