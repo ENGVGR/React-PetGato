4cc287d9ad991c6aa1a1a2ee9babc2419b7c1687
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-25 12:25:55 -0300

    A navBar receve os textos e os textos destacados por props

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,19 +2,19 @@ import logo from "../../assets/gatinho_petgato_branco.svg"
 import "./index.scss"
 import { Link } from "react-router-dom"
 
-const NavBar = () => {
+const NavBar = ({props}) => {
     return(
         <div className="navbar-div">
             <img className="navbar-img" src={logo} alt="logo"/>
             <div className="navbar-div-link">
-                <Link className="navbar-link" to="/">Página inicial</Link>
-                <Link className="navbar-link" to="/">Sobre nós</Link>
-                <Link className="navbar-link" to="/">Fale conosco</Link>
-                <Link className="navbar-link-2" to="/">Minha conta</Link>
+                <Link className={props.emphasis_t1?"navbar-link-2":"navbar-link"} to={props.link_1}>{props.text_1}</Link>
+                <Link className={props.emphasis_t2?"navbar-link-2":"navbar-link"} to={props.link_2}>{props.text_2}</Link>
+                <Link className={props.emphasis_t3?"navbar-link-2":"navbar-link"} to={props.link_3}>{props.text_3}</Link>
+                <Link className={props.emphasis_t4?"navbar-link-2":"navbar-link"} to={props.link_4}>{props.text_4}</Link>
             </div>
             
         </div>
     )
 }
 
-export {NavBar}
+export { NavBar }
