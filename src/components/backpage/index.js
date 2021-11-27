d344d700747766f25fb4394ddfd02568d09a3975
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-27 13:13:11 -0300

    backPage agora e um link. implementado input na specificPost

--- src/components/backpage/index.js
+++ src/components/backpage/index.js
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'
 import arrow from '../../assets/icons/Icon awesome-chevron-left.svg'
 import './index.scss'
 
 const BackPage = () => {
     return (
         <div className="backPage">
-            <img className="backPage__image" src={arrow} alt="arrow"/>
-            <span className="backPage__text">VOLTAR</span>
+            <Link to="/" className="backPage__link">
+                <img className="backPage__link-image" src={arrow} alt="arrow"/>
+                <span className="backPage__link-text">VOLTAR</span>
+            </Link>
         </div>
     )
 }
--- src/pages/specificPost/index.js
+++ src/pages/specificPost/index.js
@@ -6,54 +6,60 @@ import './index.scss'
 
 export default function SpecificPost({props}) {
 
-
     return (
-        <div className="specificPost">
+        <div className="body-specificPost">
             <NavBar props={paramsNavbar}/>
-            <div className="specificPost__backPost">
-                <BackPage/>
-            </div>
-            <div className="specificPost__title">
-                <span className="specificPost__title-text">As almofadinhas são importantes: guia definitivo de cuidados com as patas</span>
-            </div>
-            <div className="specificPost__subTitle">
-                <span className="specificPost__subTitle-text">Publicado em 08 de outubro de 2019 às 09h28</span>
-                <div className="specificPost__subTitle-views">
-                    <img className="specificPost__subTitle-views__image" src={views} alt="views"/>
-                    <span className="specificPost__subTitle-views__number">88</span>
+            <div className="specificPost">
+                <div className="specificPostLeft">        
+                    <div className="specificPostLeft__backPost">
+                        <BackPage/>
+                    </div>
+                    <div className="specificPostLeft__title">
+                        <span className="specificPostLeft__title-text">As almofadinhas são importantes: guia definitivo de cuidados com as patas</span>
+                    </div>
+                    <div className="specificPostLeft__subTitle">
+                        <span className="specificPostLeft__subTitle-text">Publicado em 08 de outubro de 2019 às 09h28</span>
+                        <div className="specificPostLeft__subTitle-views">
+                            <img className="specificPostLeft__subTitle-views__image" src={views} alt="views"/>
+                            <span className="specificPostLeft__subTitle-views__number">88</span>
+                        </div>
+                    </div>
+                    <img className="specificPostLeft__banner" src={banner} alt="banner"/>
+                    <div className="specificPostLeft__content">
+                        <p className="specificPostLeft__content-text">
+                            São 3 da tarde e vocÊ já pefa a coleira para passear com seu companheiro de 
+                            caminhada. Tá aquele sol do cão e o asfalto parece que dá para fritar um ovo. 
+                            Se dá pra fazer uma fritura, imagina o que isso pode fazer com a patinha do seu pet? 
+                            Essa é só uma das situações em que p yamin fica com muita preguiça de continuar o texto.
+                        </p>
+                        <p className="specificPostLeft__content-text">
+                            então o que ele faz? Completa todo o resto com Lorem Ipsum
+                        </p>
+                        <p className="specificPostLeft__content-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis laoreet tortor. Nulla 
+                            venenatis sed nulla id imperdiet. Pellentesque eu massa pulvinar, laoreet odio ac, tincidunt odio. 
+                            Curabitur viverra dui nibh, sit amet congue erat ornare non. Vestibulum posuere tellus vel porta viverra. 
+                            Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent 
+                            per conubia nostra, per inceptos himenaeos.
+                        </p>
+                        <p className="specificPostLeft__content-text">
+                            "Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent"
+                        </p>
+                        <p className="specificPostLeft__content-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis laoreet tortor. Nulla 
+                            venenatis sed nulla id imperdiet. Pellentesque eu massa pulvinar, laoreet odio ac, tincidunt odio. 
+                            Curabitur viverra dui nibh, sit amet congue erat ornare non. Vestibulum posuere tellus vel porta viverra. 
+                            Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent 
+                            per conubia nostra, per inceptos himenaeos.
+                        </p>
+                    </div>
+                    <div className="specificPostLeft__line"/>
+                </div>
+                <div className="specificPostRight">
+                    <input className="specificPostRight__search"/>
                 </div>
             </div>
-            <img className="specificPost__banner" src={banner} alt="banner"/>
-            <div className="specificPost__content">
-                <p className="specificPost__content-text">
-                    São 3 da tarde e vocÊ já pefa a coleira para passear com seu companheiro de 
-                    caminhada. Tá aquele sol do cão e o asfalto parece que dá para fritar um ovo. 
-                    Se dá pra fazer uma fritura, imagina o que isso pode fazer com a patinha do seu pet? 
-                    Essa é só uma das situações em que p yamin fica com muita preguiça de continuar o texto.
-                </p>
-                <p className="specificPost__content-text">
-                    então o que ele faz? Completa todo o resto com Lorem Ipsum
-                </p>
-                <p className="specificPost__content-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis laoreet tortor. Nulla 
-                    venenatis sed nulla id imperdiet. Pellentesque eu massa pulvinar, laoreet odio ac, tincidunt odio. 
-                    Curabitur viverra dui nibh, sit amet congue erat ornare non. Vestibulum posuere tellus vel porta viverra. 
-                    Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent 
-                    per conubia nostra, per inceptos himenaeos.
-                </p>
-                <p className="specificPost__content-text">
-                    "Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent"
-                </p>
-                <p className="specificPost__content-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis laoreet tortor. Nulla 
-                    venenatis sed nulla id imperdiet. Pellentesque eu massa pulvinar, laoreet odio ac, tincidunt odio. 
-                    Curabitur viverra dui nibh, sit amet congue erat ornare non. Vestibulum posuere tellus vel porta viverra. 
-                    Etiam enim velit, ultrices non dolor quis, volutpat iaculis orci. Class aptent taciti sociosqu ad litora torquent 
-                    per conubia nostra, per inceptos himenaeos.
-                </p>
-            </div>
         </div>
-
     )
 }
 
