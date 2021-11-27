ceaad64b9d38b50ebb62fef7e1cb46462bad4350
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-27 11:05:56 -0300

    Implementaçao do titulo e subtitulo

--- src/pages/specificPost/index.js
+++ src/pages/specificPost/index.js
@@ -1,8 +1,9 @@
 import { BackPage } from '../../components/backpage'
 import { NavBar } from '../../components/navbar'
+import views from '../../assets/icons/Icon awesome-eye.svg'
 import './index.scss'
 
-export default function SpecificPost() {
+export default function SpecificPost({props}) {
 
 
     return (
@@ -10,6 +11,16 @@ export default function SpecificPost() {
             <NavBar props={paramsNavbar}/>
             <div className="specificPost__backPost">
                 <BackPage/>
+            </div>
+            <div className="specificPost__title">
+                <span className="specificPost__title-text">As almofadinhas são importantes: guia definitivo de cuidados com as patas</span>
+            </div>
+            <div className="specificPost__subTitle">
+                <span className="specificPost__subTitle-text">Publicado em 08 de outubro de 2019 às 09h28</span>
+                <div className="specificPost__subTitle-views">
+                    <img className="specificPost__subTitle-views__image" src={views} alt="views"/>
+                    <span className="specificPost__subTitle-views__number">88</span>
+                </div>
             </div>
             
         </div>
