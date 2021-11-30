3eacb147b7f0310a4aea90cea4c31d3775c0a618
Vinícius Ribeiro <engvgr@gmail.com>
2021-11-30 18:03:41 -0300

    A funçao de obter a quantidade de likes (GetLike) agora esta em action, e nao mais em posts. Foi implementado uma funçao que Cria o like pela api (PostLike) e outra que, ao iniciar a pagina, da setClick caso o usuario ja tenha dado like (Like)

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,18 +1,58 @@
 import like from "../../assets/icons/Icon awesome-heart-1.svg"
 import like_red from "../../assets/icons/Icon awesome-heart.svg"
-import message from "../../assets/icons/Icon awesome-comment-alt.svg"
+import comment from "../../assets/icons/Icon awesome-comment-alt.svg"
 import view from "../../assets/icons/Icon awesome-eye.svg"
 import "./index.scss"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import api from "../../api/api"
 
-const Like = ({likes}) => {
+const Like = ({user_id, post_id}) => {
 
     const [Click,setClick] = useState(false)
+    const [Likes,setLikes] = useState(0)
+
+    function PostLike(user_id,post_id) {
+        
+        if (user_id ) {
+            api.post(`/post/${user_id}/${post_id}`)
+            .then((resp) => {
+                if (resp.data === "Criado") {
+                    setClick(true)                
+                }
+                else {
+                    setClick(false)
+                }
+            })
+        }
+    }
+
+    useEffect(() => {
+
+        async function GetLikes() {
+
+            api.get(`/post/${post_id}`)
+            .then((resp) => {     
+                setLikes(resp.data)
+            })
+        } GetLikes()
+
+        async function Like() {
+            api.get(`/post/${user_id}/${post_id}`)
+            .then ((resp) => {
+                if (resp.data === "Deu like!") {
+                    setClick(true)
+                }
+                else {
+                    setClick(false)
+                }
+            })
+        } Like()
+    })
 
     return (
         <div className="actions-main">
-            <input type="image" src={Click?like_red:like} alt="like" onClick={Click?() => {setClick(false)}:() => {setClick(true)}}/>
-            <span className="main-span">{likes}</span>
+            <input type="image" src={Click?like_red:like} alt="like" onClick={() => {PostLike(user_id,post_id)}}/>
+            <span className="main-span">{Likes}</span>
         </div>
     )
 }
@@ -20,7 +60,7 @@ const Like = ({likes}) => {
 const Message = ({messages}) => {
     return (
         <div className="actions-main">
-            <input type="image" src={message} alt="message"/>
+            <input type="image" src={comment} alt="message"/>
             <span className="main-span">{messages}</span>
         </div>
     )
