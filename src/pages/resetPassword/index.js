b9cb5f69db251baee075e37a717f3fd7e87838ce
Vinícius Ribeiro <engvgr@gmail.com>
2021-12-04 12:57:21 -0300

    resetPassword - Responsividade e polimento

--- src/pages/resetPassword/index.js
+++ src/pages/resetPassword/index.js
@@ -1,25 +1,28 @@
+//SCSS
 import './index.scss'
-import { useNavigate } from "react-router";
+//React
+import { useState } from 'react'
+import { useNavigate, useParams } from "react-router";
+//Components
 import { Logo } from "../../components/logo"
-import Foto_Esqueci_Minha_Senha from '../../assets/Esqueci minha senha.jpg'
 import { Input, Label } from '../../components/input'
 import { ButtonPurple } from '../../components/button'
-import { useState } from 'react'
+//Assets
+import Foto_Esqueci_Minha_Senha from '../../assets/Esqueci minha senha.jpg'
+//Api
 import api from '../../api/api';
-import { useParams } from 'react-router'
 
-function ResetPassword() {
+export default function ResetPassword() {
 
     const [Password, setPassword] = useState()
     const [PasswordConfirm, setPasswordConfirm] = useState()
     const [ErrorPassword, setErrorPassword] = useState(false)
     const [ErrorExp, setErrorExp] = useState(false)
-    const [Confirmation, setConfirmation] = useState(false)
     const { id, token } = useParams()  
     const navigate = useNavigate(); 
 
-    function handleSubmit(event) {
-        event.preventDefault()
+    function handleSubmit(e) {
+        e.preventDefault()
         
         Password===PasswordConfirm?setErrorPassword(false):setErrorPassword(true)
 
@@ -30,41 +33,29 @@ function ResetPassword() {
             }
 
             api.patch(`recuperar-senha/${id}/${token}`, Data)
-            .then((resp) => {
-                console.log(resp)
-                setConfirmation(true)
+            .then(() => {
                 navigate("/login")
             })
-            .catch((err) => {
-                console.log(err)
+            .catch(() => {
                 setErrorExp(true)
             })
-        }
-        
-        if (ErrorPassword === false) {
-            post()
-        }
+        }if (Password===PasswordConfirm) { post() }
     }
 
-    return(
-        
-        <div className="reset-password-div">
-        <img className="reset-password-image" src={Foto_Esqueci_Minha_Senha} alt="foto"/>
-        <div className="reset-password-div-form">
-            <Logo/>
-            <form onSubmit={handleSubmit}>
-                <Label htmlFor="password">Senha</Label>
-                <Input id="password" type="password" value={Password} onChange={(event) => {setPassword(event.target.value)}} required minLength="8"/>
-                <Label htmlFor="password_confirm">Confirme sua senha{ErrorPassword?<span className="reset-password-span-error"> *Precisa ser a mesma senha!</span>:<></>}</Label>
-                <Input id="password_confirm" type="password" value={PasswordConfirm} onChange={(event) => {setPasswordConfirm(event.target.value)}} required minLength="8"/><br/>
-                {ErrorExp?<span className="reset-password-span-error"> *Token já utilizado ou expirado!</span>:<></>}
-                {Confirmation?<span className="reset-password-span-error"> *Senha alterada</span>:<></>}
-                <ButtonPurple>ALTERAR A SENHA</ButtonPurple>
-            </form>
+    return(       
+        <div className="resetPassword">
+            <img className="resetPassword__image" src={Foto_Esqueci_Minha_Senha} alt="foto"/>
+            <div className="resetPassword__form">
+                <Logo/>
+                <form onSubmit={handleSubmit}>
+                    <Label htmlFor="password">Senha</Label>
+                    <Input id="password" type="password" value={Password} onChange={(event) => {setPassword(event.target.value)}} required minLength="8"/>
+                    <Label htmlFor="password_confirm">Confirme sua senha{ErrorPassword?<span className="resetPassword__form-error"> *Precisa ser a mesma senha!</span>:<></>}</Label>
+                    <Input id="password_confirm" type="password" value={PasswordConfirm} onChange={(event) => {setPasswordConfirm(event.target.value)}} required minLength="8"/><br/>
+                    {ErrorExp?<span className="resetPassword__form-error"> *Token já utilizado ou expirado!</span>:<></>}
+                    <ButtonPurple>ALTERAR A SENHA</ButtonPurple>
+                </form>
+            </div>
         </div>
-    </div>
-
     )
 }
-
-export default ResetPassword
